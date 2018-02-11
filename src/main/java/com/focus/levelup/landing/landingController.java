9a8f3a99e6815583bada9f35ab14585d3673c626
rafael.gutierrez <rgutierreztejada@gmail.com>
2018-02-10 21:36:36 -0600

    landing

--- src/main/java/com/focus/levelup/landing/landingController.java
+++ src/main/java/com/focus/levelup/landing/landingController.java
@@ -9,6 +9,31 @@ public class landingController {
 
 	@RequestMapping("index")
 	public String landing() {
+		
 		return "landing/index";
+	}
+	
+	@RequestMapping("about")
+	public String about() {
+		
+		return "landing/about";
+	}
+	
+	@RequestMapping("contact-us")
+	public String contactus() {
+		
+		return "landing/contact-us";
+	}
+	
+	@RequestMapping("login")
+	public String login() {
+		
+		return "landing/login";
+	}
+	
+	@RequestMapping("pricing")
+	public String pricing() {
+		
+		return "landing/pricing";
 	}
 }
