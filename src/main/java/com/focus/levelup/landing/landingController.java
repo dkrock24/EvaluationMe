abe57eb5d38ad8a339e0ef5c412bc66e88f48258
rafael.gutierrez <rgutierreztejada@gmail.com>
2018-02-25 11:09:14 -0600

    pricing

--- src/main/java/com/focus/levelup/landing/landingController.java
+++ src/main/java/com/focus/levelup/landing/landingController.java
@@ -1,13 +1,27 @@
 package com.focus.levelup.landing;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.focus.levelup.model.Plane;
+import com.focus.levelup.model.PlanesDetail;
+import com.focus.levelup.services.PricingDetailService;
+import com.focus.levelup.services.PricingService;
 
 @Controller
 @RequestMapping("landing")
 public class landingController {
 	
+	@Autowired
+	PricingService pricingService;
+	
+	@Autowired
+	PricingDetailService pricingDetail;
 
 
 	@GetMapping("index")
@@ -40,8 +54,11 @@ public class landingController {
 	public String pricing(Model model) {
 		
 		List<Plane> pricing = (List<Plane>) pricingService.findAll();
+		List<PlanesDetail> pricingDetails = (List<PlanesDetail>) pricingDetail.findAll();
 		
 		model.addAttribute("pricing", pricing);
+		model.addAttribute("pricingDetail", pricingDetails);
+		
 		
 		return "landing/pricing";
 	}
---
+++ src/main/java/com/focus/levelup/services/PricingDetailService.java
@@ -0,0 +1,9 @@
+package com.focus.levelup.services;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.focus.levelup.model.PlanesDetail;
+
+public interface PricingDetailService extends CrudRepository<PlanesDetail, Integer>{
+
+}
