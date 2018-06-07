dd8ec739fb86f0ea31a89912daec6ae2b21320c3
dkrock24 <rgutierreztejada@gmail.com>
2018-06-06 20:49:40 -0600

    Ajustando Login

--- src/main/java/com/focus/levelup/controller/LoginController.java
+++ src/main/java/com/focus/levelup/controller/LoginController.java
@@ -1,35 +1,50 @@
 package com.focus.levelup.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.focus.levelup.model.User;
 import com.focus.levelup.services.LoginService;
 
+import ch.qos.logback.core.net.LoginAuthenticator;
+
 @Controller
+@RequestMapping("login")
 public class LoginController {
 	
-	//@Autowired
-	//LoginService loginService;
+	@Autowired
+	LoginService loginService;
 
-	@RequestMapping("Login")
+	@RequestMapping("index")
 	public String login() {		
 		return "backend/login/Login";
 		
 	}
 	
 	@RequestMapping("authentication")
-	public String authentication(Model model, @RequestParam String email,@RequestParam String password) {
+	public ModelAndView authentication(Model model, @RequestParam String email,@RequestParam String password) {
 		
-		//Users user = (Users) loginService.loginAuthentication( email, "$2a$10$5I3f3fLKvAEjEvYtCqU0NeHu5MyxkXB.To.2EmIOBjuANDzln4PF6" );
-		//if( user != null ) {
-			//System.out.println( user.getFirstName() );
-		//}
+		String pagina="";
+		List<User> user = loginService.loginAuthentication(email, "$2a$10$hnC.Foxw7GWja0pNIR58vevLgXrI4N8viYt/PCFeHS8sWjTgRckMy" );
 		
-		return "backend/login/Login";
+		if( !user.isEmpty() ) {
+			
+			pagina = "redirect:/Dashboard/index";
+			for(User u : user) {
+				model.addAttribute("name",u.getFirstName());
+			}
+		}else {
+			pagina = "redirect:/login/index";
+		}
+
+		return new ModelAndView(pagina);
 		
 	}
 }
--- src/main/java/com/focus/levelup/services/LoginService.java
+++ src/main/java/com/focus/levelup/services/LoginService.java
@@ -2,6 +2,7 @@ package com.focus.levelup.services;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -10,4 +11,10 @@ import com.focus.levelup.model.User;
 public interface LoginService extends CrudRepository<User, Integer>{
 	
 	//public User loginAuthentication(@Param("email") String email, @Param("passwd") String passwd);
+	
+	@Query("SELECT u FROM User u WHERE u.email = :email and u.password = :passwd")
+	List<User> loginAuthentication(@Param("email") String email, @Param("passwd") String passwd);
+	
+	//@Query("SELECT u FROM User u")
+	//List<User> DemoUser();
 }
